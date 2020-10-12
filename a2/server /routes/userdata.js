e7e9f9ef1e95e858a7fac233eb72922b323b1ac9
Gaganpreet Singh <gaganpreetsingh@griffithuni.edu.au>
2020-10-12 20:40:45 +1000

    user data array checking

--- a2/server /routes/userdata.js
+++ a2/server /routes/userdata.js
@@ -1,4 +1,5 @@
 module.exports = function(db, app) {
+  //This route will return the user data from an array 
   app.get('/api/userdata', function(req, res) {
     const collection = db.collection('users')
     collection.find({}).toArray((err, data) => {
