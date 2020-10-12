611d86aa2197fe4cb433fe04f538874100e4ec38
Gaganpreet Singh <gaganpreetsingh@griffithuni.edu.au>
2020-10-12 20:41:26 +1000

    user data arrg

--- a2/server /routes/showgroup.js
+++ a2/server /routes/showgroup.js
@@ -1,4 +1,4 @@
-module.exports = function(db, app) { // Route and method to show group from an array
+module.exports = function(db, app) { // Route and method to show group from an array 
   app.get('/api/showgroup', function(req, res) {
     const collection = db.collection('groups')
     collection.find({}).toArray((err, data) => {
